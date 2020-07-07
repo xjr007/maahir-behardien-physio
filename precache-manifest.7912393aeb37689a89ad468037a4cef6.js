405fc2f484419410572f0e127259794cacad3f95
Aaqiel <abaaqielbehardien@gmail.com>
2020-07-07 16:41:49 +0200

    Updates

---
+++ precache-manifest.7912393aeb37689a89ad468037a4cef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d4f681c65f3613d9f464f17876d613",
+    "url": "/index.html"
+  },
+  {
+    "revision": "964d5d6904a689d85ab0",
+    "url": "/static/css/main.04eb33fa.chunk.css"
+  },
+  {
+    "revision": "37111d9bcf4c7cd5f1b7",
+    "url": "/static/js/2.a4a913e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4a913e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964d5d6904a689d85ab0",
+    "url": "/static/js/main.6c2558ee.chunk.js"
+  },
+  {
+    "revision": "f440f95f347cdffbcdad",
+    "url": "/static/js/runtime-main.1e7157ab.js"
+  }
+]);
