1fc26f9c52b642673bf3d503d1c4a1acee307f0e
Aaqiel <abaaqielbehardien@gmail.com>
2020-07-08 12:58:59 +0200

    Updates

---
+++ precache-manifest.d1a3291099913a01a4a26dc09d3512c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1681541d5bd45a8788fb3c9ddef25b5f",
+    "url": "/maahir-behardien-physio/index.html"
+  },
+  {
+    "revision": "8a2b29984b96b50b9cea",
+    "url": "/maahir-behardien-physio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1057a954fc69c0cab144",
+    "url": "/maahir-behardien-physio/static/css/main.7adfdf4f.chunk.css"
+  },
+  {
+    "revision": "8a2b29984b96b50b9cea",
+    "url": "/maahir-behardien-physio/static/js/2.6c2e6d96.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/maahir-behardien-physio/static/js/2.6c2e6d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1057a954fc69c0cab144",
+    "url": "/maahir-behardien-physio/static/js/main.0ec82aff.chunk.js"
+  },
+  {
+    "revision": "96e05b418f12ee79b383",
+    "url": "/maahir-behardien-physio/static/js/runtime-main.5c7de229.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d87413f0713efc92503c41850765a80e.js"
+  "/maahir-behardien-physio/precache-manifest.d1a3291099913a01a4a26dc09d3512c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/maahir-behardien-physio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
