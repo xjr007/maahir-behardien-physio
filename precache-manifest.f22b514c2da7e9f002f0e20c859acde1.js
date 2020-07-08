96cc11ef41b503250c84ddf85c1a82b0ec51475f
Aaqiel <abaaqielbehardien@gmail.com>
2020-07-08 13:37:21 +0200

    Updates

--- precache-manifest.f22b514c2da7e9f002f0e20c859acde1.js
+++ precache-manifest.f22b514c2da7e9f002f0e20c859acde1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f9bd5505028a995041592ceb139f1335",
+    "revision": "747de737175749c29545744c42d3248c",
     "url": "/maahir-behardien-physio/index.html"
   },
   {
-    "revision": "8a2b29984b96b50b9cea",
+    "revision": "49358a7a49f0978aca37",
     "url": "/maahir-behardien-physio/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "51dde3877b7b5236dc94",
+    "revision": "985be3dca0b6bb1356ab",
     "url": "/maahir-behardien-physio/static/css/main.7adfdf4f.chunk.css"
   },
   {
-    "revision": "8a2b29984b96b50b9cea",
-    "url": "/maahir-behardien-physio/static/js/2.6c2e6d96.chunk.js"
+    "revision": "49358a7a49f0978aca37",
+    "url": "/maahir-behardien-physio/static/js/2.1186d3a6.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/maahir-behardien-physio/static/js/2.6c2e6d96.chunk.js.LICENSE.txt"
+    "url": "/maahir-behardien-physio/static/js/2.1186d3a6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "51dde3877b7b5236dc94",
-    "url": "/maahir-behardien-physio/static/js/main.ad529117.chunk.js"
+    "revision": "985be3dca0b6bb1356ab",
+    "url": "/maahir-behardien-physio/static/js/main.a68cf407.chunk.js"
   },
   {
     "revision": "96e05b418f12ee79b383",
